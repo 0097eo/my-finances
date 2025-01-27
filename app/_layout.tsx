738847ba4c3f0d1e55b889975310ca579f279aa1
0097eo <emmanuelokello294@gmail.com>
2025-01-27 11:03:11 +0300

    Wrap RootLayout in AuthProvider for user authentication context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from 'expo-router';
+import { AuthProvider } from '@/components/AuthContext';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false}}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
   );
 }
