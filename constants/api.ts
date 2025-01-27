394f7712654e6cb98903a316a89814e79e85ad69
0097eo <emmanuelokello294@gmail.com>
2025-01-27 11:02:47 +0300

    Update API_URL to support different environments for Android and iOS

--- constants/api.ts
+++ constants/api.ts
@@ -1,1 +1,14 @@
-export const API_URL = 'http://127.0.0.1:5555/api';
+import { Platform } from "react-native";
+
+const getApiUrl = () => {
+    if (__DEV__) {
+      if (Platform.OS === 'android') {
+        return 'http://192.168.0.102:5555/';
+      }
+      if (Platform.OS === 'ios') {
+        return 'http://localhost:5555/';
+      }
+    }
+  };
+  
+  export const API_URL = getApiUrl();
